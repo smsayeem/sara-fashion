84b492a9351bd6dc7ef662d0ed471cfaa4b7d60f
Sam Sadot <sam.sadot@sca.com.au>
2020-04-18 18:43:52 +1000

    installed redux-persist library to manage our loca storage session in the web page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
+// redux-persist: it allows browser to cache our store
+import { persistStore } from 'redux-persist'
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(...middlewares))
 );
+export const persistor = persistStore(store);
 
-export default store;
+// export default store;
